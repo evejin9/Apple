28d661158eea3cd76e82d7c8d2f2359f5788eddd
evejin9 <evejin9@naver.com>
2023-04-18 18:17:42 +0900

    23.04.18) 애플 클론 코딩
    - 메뉴 선택 시 헤더 색상 바꾸기
    - 슬라이드 오버플로우 주기
    - 페이지네이션 색상 바꾸기 (항상 오타 주의)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,62 @@
+const headerEl = document.querySelector('header');
+const menuEl = document.querySelector('header .menu');
+const searchMenuEl = document.querySelector('header .search-menu');
+const shopMenuEl = document.querySelector('header .shop-menu');
+const clickSearch = document.querySelector('header .item__name.search');
+const clickShop = document.querySelector('header .item__name.shop');
+
+// a태그에 포커스 됐을 때 효과주고, 블러 됐을 때 효과 주는 것으로 바꿔보기
+
+clickSearch.addEventListener('focus', function () {
+  headerEl.style.backgroundColor = '#161617';
+  headerEl.style.transition = '.4s .4s';
+})
+clickSearch.addEventListener('blur', function () {
+  headerEl.style.backgroundColor = 'rgba(22, 22, 23, .8)';
+  headerEl.style.transition = '';
+} );
+
+clickShop.addEventListener('focus', function () {
+  headerEl.style.backgroundColor = '#161617';
+  headerEl.style.transition = '.4s .4s';
+})
+clickShop.addEventListener('blur', function () {
+  headerEl.style.backgroundColor = 'rgba(22, 22, 23, .8)';
+  headerEl.style.transition = '';
+} );
+
+
+// item__name을 눌렀을 때 실행
+// item__name을 누르면 헤더의 배경색을 #161617 으로 바꾸기
+
+// menuEl.addEventListener('mouseover', function () {
+//   headerEl.style.backgroundColor = '#161617';
+//   headerEl.style.transition = '.4s .4s';
+  
+// });
+// menuEl.addEventListener('mouseout', function () {
+//   headerEl.style.backgroundColor = 'rgba(22, 22, 23, .8)';
+//   headerEl.style.transition = '';
+// });
+
+// searchMenuEl.addEventListener('click', function () {
+//   headerEl.style.backgroundColor = '#161617';
+//   headerEl.style.transition = '.4s .4s';
+// });
+// searchMenuEl.addEventListener('mousedown', function () {
+//     headerEl.style.backgroundColor = 'rgba(22, 22, 23, .8)'
+//     headerEl.style.transition = '';
+//   });
+  
+//   shopMenuEl.addEventListener('click', function () {
+//     headerEl.style.backgroundColor = '#161617';
+//     headerEl.style.transition = '.4s .4s';
+//   });
+//   shopMenuEl.addEventListener('mousedown', function () {
+//     headerEl.style.backgroundColor = 'rgba(22, 22, 23, .8)'
+//     headerEl.style.transition = '';
+//   });
+
 new Swiper('.today_color .swiper', {
   direction: 'vertical',
   loop: true,
